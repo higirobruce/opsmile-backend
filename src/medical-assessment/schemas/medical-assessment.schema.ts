97721bb5648a554f5d4fbea99bf2afdbcf99753c
Bruce <higirobru@gmail.com>
2025-09-05 14:44:24 +0200

    Lab requests and results

--- src/medical-assessment/schemas/medical-assessment.schema.ts
+++ src/medical-assessment/schemas/medical-assessment.schema.ts
@@ -10,38 +10,68 @@ export enum SurgicalDecision {
   PENDING = 'pending'
 }
 
+export enum SampleType {
+  BLOOD = 'blood',
+  URINE = 'urine',
+  SKIN = 'skin',
+  NAIL = 'nail',
+  TEETH = 'teeth',
+  OTHER = 'other'
+}
+
+export interface Sample {
+  sampleType: SampleType,
+  collectionDate: Date,
+  status: string
+}
+
+export interface LabResult{
+  testName: string,
+  result: string,
+  unit: string,
+  referenceRange: string,
+  resultDate: Date
+}
+
+export interface LabRequest {
+  status: string;
+  tests: string[];
+  samples: Sample[];
+  results: LabResult[];
+}
+
 export type MedicalAssessmentDocument = MedicalAssessment & Document;
 
 @Schema({ timestamps: true })
 export class MedicalAssessment {
   @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'Patient', required: true })
   patient: Patient;
-  
+
   @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'User', required: true })
   doneBy: User;
-  
+
   @Prop({ default: Date.now })
   assessmentDate: Date;
-  
+
   @Prop(String)
   pastMedicalHistory: string;
 
-  @Prop([String])
-  labRequests: string[];
-  
+  @Prop([Object])
+  labRequests: LabRequest[];
+
   @Prop(String)
   @Prop({ required: true })
   diagnosis: string;
-  
+
   @Prop({ type: [Object] })
   uploadedFiles: ConsentFile[];
-  
+
   @Prop({ type: [Object] })
   uploadedPhotos: ConsentFile[];
-  
+
   @Prop({ type: Boolean })
   clearedForSurgery: boolean;
-  
+
   @Prop({ type: String })
   reasonForCancellation: string
 }
